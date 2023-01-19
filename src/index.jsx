a90c02357c351331b0278a4714800263bf05e208
Smael Nicolas <nico.smael@gmail.com>
2023-01-19 12:35:45 -0300

    imported components to ap

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,67 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { styles } from "../styles";
+import { AddItem, CustomModal, TaskList } from "./components";
 
 export const App = () => {
+	const [task, setTask] = useState();
+	const [tasks, setTasks] = useState([]);
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const [selectedTask, setSelectedTask] = useState({});
+
+	const onHandlerChange = (text) => {
+		setTask(text);
+	};
+
+	const onHandlerSubmit = () => {
+		setTasks([
+			...tasks,
+			{
+				id: Math.random().toString(),
+				value: task,
+				complete: false,
+			},
+		]);
+		setTask("");
+	};
+
+	const onHandleModal = (item) => {
+		setSelectedTask(item);
+		setIsModalVisible(!isModalVisible);
+	};
+
+	const onHandleDelete = () => {
+		setTasks((prevTaskList) =>
+			prevTaskList.filter((task) => task.id !== selectedTask.id)
+		);
+		setIsModalVisible(!isModalVisible);
+	};
+
+	const onHandleStatus = (task) => {
+		task.complete = !task.complete;
+	};
+
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style="auto" />
+			<AddItem
+				placeholder={"Add a new task"}
+				task={task}
+				onHandlerChange={onHandlerChange}
+				onHandlerSubmit={onHandlerSubmit}
+				buttonText={"ADD"}
+			/>
+			<TaskList
+				tasks={tasks}
+				onHandleModal={onHandleModal}
+				style={styles.listContainer}
+			/>
+			<CustomModal
+				isModalVisible={isModalVisible}
+				setIsModalVisible={setIsModalVisible}
+				onHandleDelete={onHandleDelete}
+				onHandleStatus={onHandleStatus}
+				selectedTask={selectedTask}
+			/>
 		</View>
 	);
 };
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
