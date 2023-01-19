160cd30e3143278c60f44ae614742e77f0e3b7e8
Smael Nicolas <nico.smael@gmail.com>
2023-01-19 12:19:59 -0300

    added folder structure

---
+++ src/components/Customs/Modal/CustomModal.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CustomModal = () => {
+	return <div>CustomModal</div>;
+};
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { AddItem } from "./AddItem/AddItem";
+export { CustomModal } from "./Customs/Modal/CustomModal";
+export { TaskItem } from "./Task/TaskItem/TaskItem";
+export { TaskList } from "./Task/TaskList/TaskList";
