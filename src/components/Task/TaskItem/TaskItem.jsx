04397f9e45ac60048bb4a4b6e6fe8e4c2b175583
Smael Nicolas <nico.smael@gmail.com>
2023-01-19 12:35:14 -0300

    addded task render

--- src/components/Task/TaskItem/TaskItem.jsx
+++ src/components/Task/TaskItem/TaskItem.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { styles } from "../../../../styles";
 
-export const TaskItem = () => {
-	return <div>TaskItem</div>;
+export const TaskItem = ({ item, onHandleModal }) => {
+	return (
+		<TouchableOpacity
+			style={
+				item.complete
+					? styles.itemContainerComplete
+					: styles.itemContainer
+			}
+			onPress={() => onHandleModal(item)}>
+			<Text style={styles.itemList}>asda</Text>
+		</TouchableOpacity>
+	);
 };
