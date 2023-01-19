2cc76ea7c6b8975ce53a02a31d5c7653e256c349
Smael Nicolas <nico.smael@gmail.com>
2023-01-19 16:18:46 -0300

    add export title

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { AddItem } from "./AddItem/AddItem";
 export { CustomModal } from "./Customs/Modal/CustomModal";
 export { TaskItem } from "./Task/TaskItem/TaskItem";
 export { TaskList } from "./Task/TaskList/TaskList";
+export { Title } from "./Title/Title";
