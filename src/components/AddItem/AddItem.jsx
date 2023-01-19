e6eb128b3e7e5fb6293d5fc7adbd74f686210100
Smael Nicolas <nico.smael@gmail.com>
2023-01-19 12:34:42 -0300

    created textinput and button to add task

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { Button, TextInput, View } from "react-native";
+import { styles } from "../../../styles";
 
-export const AddItem = () => {
-	return <div>AddItem</div>;
+export const AddItem = ({
+	placeholder,
+	task,
+	onHandlerChange,
+	onHandlerSubmit,
+	buttonText,
+}) => {
+	return (
+		<View style={styles.inputContainer}>
+			<TextInput
+				style={styles.input}
+				placeholder={placeholder}
+				autoComplete="off"
+				autoCorrect={false}
+				autoCapitalize="none"
+				value={task}
+				onChangeText={(e) => onHandlerChange(e)}
+			/>
+			<Button
+				disabled={!task}
+				onPress={onHandlerSubmit}
+				title={buttonText}
+				color="#545956"
+			/>
+		</View>
+	);
 };
