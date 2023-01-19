0096d9e0b36552dd438680882390ca289dee0f72
Smael Nicolas <nico.smael@gmail.com>
2023-01-19 12:35:29 -0300

    added task list to display all tasks

--- src/components/Task/TaskList/TaskList.jsx
+++ src/components/Task/TaskList/TaskList.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { FlatList } from "react-native";
+import { styles } from "../../../../styles";
+import { TaskItem } from "../TaskItem/TaskItem";
 
-export const TaskList = () => {
-	return <div>TaskList</div>;
+export const TaskList = ({ tasks, onHandleModal }) => {
+	const renderItem = ({ item }) => (
+		<TaskItem item={item} onHandleModal={onHandleModal} />
+	);
+
+	const keyExtractor = (item) => item.id;
+
+	return (
+		<FlatList
+			data={tasks}
+			renderItem={renderItem}
+			keyExtractor={keyExtractor}
+			style={styles.listContainer}
+		/>
+	);
 };
