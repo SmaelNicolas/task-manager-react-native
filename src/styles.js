2bedb97d6ef7baee116adb09912a2cc9463ccfbe
Smael Nicolas <nico.smael@gmail.com>
2023-01-19 16:19:22 -0300

    add styles to screen container

---
+++ src/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { colors } from "./theme/colors";
+
+export const styles = StyleSheet.create({
+	container: {
+		alignItems: "center",
+		backgroundColor: colors.primary,
+		flex: 1,
+		paddingVertical: 20,
+	},
+});
